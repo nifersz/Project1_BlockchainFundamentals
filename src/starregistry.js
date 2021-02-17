6b3bc4e3bbad3e191494f87e16e078d0775454a8
Nelson Fernandez <nifersz@icloud.com>
2021-02-17 15:36:18 -0500

    Add missing implementation for block.js, blockchain.js and related files

---
+++ src/starregistry.js
@@ -0,0 +1,10 @@
+class StarRegistry {
+
+    constructor(owner, star) {
+        this.owner = owner;                                     
+        this.star = star;                    
+    }
+    
+}
+
+module.exports.StarRegistry = StarRegistry;  
